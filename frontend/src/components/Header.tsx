41cb264617a36ee7ffa1bc5dd6f32956b6d42d4f
unknown <rannnz1@gmail.com>
2024-07-22 23:20:55 +0300

    chnage logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,13 +18,15 @@ export default memo(function Header({ isDarkMode, handleToggleDarkMode, handleTo
   return (
      <AppBar position="sticky"  sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)', marginBottom: '25px' }}>
                           <Box display='flex' alignItems='center' justifyContent='space-between' pl={2}>
-                                <Box pt={1} pb={1} display='flex' alignItems='center' gap={2}>
-                                    <img src={'/assets/header-logo.png'} alt='logo' height='100px' />
-                                    <Typography sx={{ fontSize: 34, fontWeight: 'bold', mb: 1, color: 'white' }} color="text.primary" gutterBottom>
-                                      Pokedex
+                                <Box pt={1} pb={1} display='flex' alignItems='center' gap={3}>
+                                  <img src={'/assets/Pokemon.png'} alt='pokemon-ball' height='125px' />
+
+                                  <Typography sx={{ fontSize: 48, fontWeight: 'bold', color: 'white' }} color="text.primary">
+                                      Guardio Pokedex
                                     </Typography>
                                   </Box>
                                 <Box pr={2} display='flex' alignItems='center'>
+
                                 <Typography>Dark Mode Toggle</Typography>
                                   <Switch checked={isDarkMode} onChange={handleToggleDarkMode}/>
                                   <IconButton edge="end" color="inherit" aria-label="menu" onClick={() => handleToggleDrawer()}>
